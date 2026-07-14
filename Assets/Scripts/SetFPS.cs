daee901ff128cb903bb79f7bbd618b014b0def43
yokoyama1123 <it252250@trident.ac.jp>
2026-07-14 12:31:30 +0900

    0714End

---
+++ Assets/Scripts/SetFPS.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SetFPS : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+    }
+}
